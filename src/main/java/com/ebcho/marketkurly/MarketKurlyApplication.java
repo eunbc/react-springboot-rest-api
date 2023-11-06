9690b38e35c953f7eddd6d370642e99e663b74c5
ebcho <joeunbi514@naver.com>
2023-11-06 23:33:47 +0900

    init project

---
+++ src/main/java/com/ebcho/marketkurly/MarketKurlyApplication.java
@@ -0,0 +1,13 @@
+package com.ebcho.marketkurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketKurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketKurlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebcho/marketkurly/MarketKurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebcho.marketkurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketKurlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
